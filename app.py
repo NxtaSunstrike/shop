db5bbdbc0b45f21ec04f52efe22d9fe655fef94d
NxtaSunstrike <152981021+NxtaSunstrike@users.noreply.github.com>
2024-03-26 11:46:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,37 @@ from aiogram import Bot,Dispatcher
 from bot.handlers import admin, user, payment
 from bot.databases.base import async_main
 
-from bot.web_apps.shop.main import alive
+
 from bot.commands.shop_com import setup_bot_commands
 from bot.middlewares.check_in import CheckSubs
 
 logging.basicConfig(level=logging.INFO)
+from flask import Flask,render_template
+from bot.databases.requests import get_cloths,get_cloth_by_id
+from threading import Thread
+
+app = Flask(__name__)
+
+
+@app.route('/')
+async def index():
+    cloths = await get_cloths()
+    return render_template('main_page.html',cloths = cloths,enumerate=enumerate)
+
+
+@app.route('/<int:id>')
+async def cloth(id):
+    cloth = await get_cloth_by_id(id=id)
+    return render_template('cloth.html',enumerate = enumerate,cloth = cloth)
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def alive():
+  t = Thread(target=run)
+  t.start()
 
 async def main()->None:
     await async_main()
